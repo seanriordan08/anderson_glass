0820cad188bc066a75749503b5d44cde82d5c7c8
seanriordan08 <sean.riordan08@gmail.com>
2016-07-02 10:10:09 -0600

    Add responsive banner

---
+++ app/assets/javascripts/general.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $('.main_slider').slick({
+    slidesToShow: 10,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    variableWidth: true,
+    dots: true,
+    cssEase: 'linear',
+    pauseOnHover: true
+  });
+});
