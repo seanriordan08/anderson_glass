1348e92a3bb5e79a824923416d0e48406d30031c
seanriordan08 <sean.riordan08@gmail.com>
2016-10-15 19:21:04 -0600

    Add host_name to s3 configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ PAPERCLIP_STORAGE_OPTS = {
   path: Rails.root.join('pictures', ':style', ':filename' ).to_s,
   styles: { :banner => "1497x589>", medium: "300x300>", :thumb => "192x192>" },
   storage: :s3,
+  s3_host_name: 's3-us-west-2.amazonaws.com',
   s3_credentials: {
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
