625ce9c69f8d2eb27e87433f45bcbf054f392a6c
seanriordan08 <sean.riordan08@gmail.com>
2016-11-19 13:27:15 -0700

    Mess with migs

--- db/migrate/20161119155612_add_featured_to_photos.rb
+++ db/migrate/20161119155612_add_featured_to_photos.rb
@@ -3,11 +3,11 @@ class AddFeaturedToPhotos < ActiveRecord::Migration
     add_column :pictures, :featured, :boolean, default: false
   end
 
-  Picture.reset_column_information
-  # Add featured pictures for each market
-  @residential_picture = Picture.where(market: 'residential').first
-  @residential_picture.update(featured: true) if @residential_picture.present?
-
-  @commercial_picture = Picture.where(market: 'commercial').first
-  @commercial_picture.update(featured: true) if @commercial_picture.present?
+  # Picture.reset_column_information
+  # # Add featured pictures for each market
+  # @residential_picture = Picture.where(market: 'residential').first
+  # @residential_picture.update_attributes(featured: true) if @residential_picture.present?
+  #
+  # @commercial_picture = Picture.where(market: 'commercial').first
+  # @commercial_picture.update_attributes(featured: true) if @commercial_picture.present?
 end
